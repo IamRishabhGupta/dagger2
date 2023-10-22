44293a1729726d5f72112457ff2c2609adbced73
Rishabh Gupta <rishabh9aug@gmail.com>
2023-10-22 20:48:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dagger2/UserRegistrationService.kt
@@ -0,0 +1,16 @@
+package com.example.dagger
+
+import com.example.dagger2.EmailService
+
+class UserRegistrationService(private val userRepository: UserRepository,
+                              private val emailService: EmailService
+) {
+
+    fun register(email: String, password: String) {
+        userRepository.saveuser(email, password)
+        emailService.send(
+            to = email,
+            "rishabh9aug@gmail.com", "Hakuna matata", "This is a test email"
+        )
+    }
+}
